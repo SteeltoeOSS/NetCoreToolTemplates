e042781041aff76b5316d5d2d4d9e878097f4962
Chris Cheetham <chris@cheetham.com>
2021-05-07 15:25:22 -0400

    Reformat code for readability

--- test/DotNetNew.Test.Utilities/FluentExtensions.cs
+++ test/DotNetNew.Test.Utilities/FluentExtensions.cs
@@ -9,10 +9,8 @@ namespace Steeltoe.DotNetNew.Test.Utilities
         public static AndConstraint<StringAssertions> ContainSnippet(this StringAssertions assertion, string snippet)
         {
             var regex = Regex.Replace(snippet, @"\s+", @"\s+")
-                .Replace("(", @"\(")
-                .Replace(")", @"\)")
-                .Replace("[", @"\[")
-                .Replace("]", @"\]")
+                .Replace("(", @"\(").Replace(")", @"\)")
+                .Replace("[", @"\[").Replace("]", @"\]")
                 .Replace("|", @"\|")
                 .Replace(".", @"\s*\.\s*");
             assertion.Subject.Should().MatchRegex(regex);
