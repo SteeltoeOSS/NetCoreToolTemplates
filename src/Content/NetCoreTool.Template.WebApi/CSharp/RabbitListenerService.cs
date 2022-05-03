0745a64fea344158f452fe283b7f8706fc54c65b
Dave Tillman <dtillman@vmware.com>
2022-05-03 11:34:14 -0600

    Add RabbitMQ messaging to webapi template

---
+++ src/Content/NetCoreTool.Template.WebApi/CSharp/RabbitListenerService.cs
@@ -0,0 +1,24 @@
+#if (MessagingRabbitMqListenerOption)
+using Microsoft.Extensions.Logging;
+using Steeltoe.Messaging.RabbitMQ.Attributes;
+
+namespace Company.WebApplication.CS
+{
+    public class RabbitListenerService
+    {
+        public const string RECEIVE_AND_CONVERT_QUEUE = "steeltoe_message_queue";
+        private ILogger _logger;
+
+        public RabbitListenerService(ILogger<RabbitListenerService> logger)
+        {
+            _logger = logger;
+        }
+
+        [RabbitListener(RECEIVE_AND_CONVERT_QUEUE)]
+        public void ListenForAMessage(string msg)
+        {
+            _logger.LogInformation($"Received the message '{msg}' from the queue.");
+        }
+    }
+}
+#endif
