547987cd5e2d01309e5f3dff03e4c589cf7d0de0
Bart Koelman <104792814+bart-vmware@users.noreply.github.com>
2026-09-08 17:55:39 +0200

    Align with Steeltoe package updates in SteeltoeOSS/Steeltoe#1734 (#150)

--- test/NetCoreTool.Template.WebApi.Test/ConnectorMongoDbOptionTest.cs
+++ test/NetCoreTool.Template.WebApi.Test/ConnectorMongoDbOptionTest.cs
@@ -9,7 +9,7 @@ namespace Steeltoe.NetCoreTool.Template.WebApi.Test
     {
         protected override void AssertPackageReferencesHook(ProjectOptions options, List<(string, string)> packages)
         {
-            packages.Add(("MongoDB.Driver", "3.8.*"));
+            packages.Add(("MongoDB.Driver", "3.11.*"));
             packages.Add((GetPackageName(options.SteeltoeVersion), "$(SteeltoeVersion)"));
         }
 
